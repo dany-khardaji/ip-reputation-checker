5ea1ff2ab40b942b48825c675f1102daccec73f1
Dany Khardaji <danykhardaji@protonmail.com>
2026-07-23 22:15:12 -0400

    move API key out of code into .env with python dotenv module

--- check_ip.py
+++ check_ip.py
@@ -1,16 +1,22 @@
+from dotenv import load_dotenv
+import os
 import requests         # the HTTP tool you'll use to call the API
                         # You'll also need something to load your API key from a .env file later.
-                        # Leave that import out for now — we add it at Step 5, when you actually move the key out of the code. One tool at a time.
+                        
+load_dotenv()
+
 def main():
+    api_key = os.getenv("ABUSEIPDB_API_KEY")
 
     headers = {
-    "Key": "REDACTED", 
+    "Key": api_key, 
     "Accept": "application/json"
 }
     params = {
     "ipAddress": "118.25.6.39", 
     "maxAgeInDays": 90
 }
+
     response = requests.get("https://api.abuseipdb.com/api/v2/check", headers=headers, params=params)   #API get request
     response_dict = response.json()      #Parse the JSON to access abuseIPDB data
 
@@ -18,7 +24,6 @@ def main():
     country = response_dict["data"]["countryCode"]
     isp = response_dict["data"]["isp"]
     total_reports = response_dict["data"]["totalReports"]
-
     # print(response.status_code)
     # print(response.text)
     print(f"------- ip: {params['ipAddress']} ------- \nScore: {abuse_confidence_score}, \nCountry: {country}, \nISP: {isp}, \nReports: {total_reports}")
